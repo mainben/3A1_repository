3c6b814080e5a7e0bf2abc95d516e7aec87dc801
Titiloss <Titiloss@Titiloss-PC>
2014-02-16 14:10:05 +0100

    c'est la 1ere modification mainben1

--- smsApplication/src/smsapplication/Etudiant.java
+++ smsApplication/src/smsapplication/Etudiant.java
@@ -12,5 +12,31 @@ public class Etudiant {
     private int idEtudiant;
     private String nom;
     private String prenom;
+
+    public int getIdEtudiant() {
+        return idEtudiant;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setIdEtudiant(int idEtudiant) {
+        this.idEtudiant = idEtudiant;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    
+    
     
 }

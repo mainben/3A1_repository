5e09516bed30fa17373ceb1a09fa6d87bc542423
Titiloss <Titiloss@Titiloss-PC>
2014-02-16 14:06:26 +0100

    c'est la 1ere modification mainben

---
+++ smsApplication/src/smsapplication/Etudiant.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smsapplication;
+
+/**
+ *
+ * @author Titiloss
+ */
+public class Etudiant {
+    private int idEtudiant;
+    private String nom;
+    private String prenom;
+    
+}
